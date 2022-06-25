70fe6ffcc2354d0a4ebc4fd0dd4eb9ff78d3af81
dinggasukapaus <182410102006@mail.unej.ac.id>
2022-06-25 16:22:34 +0700

    route contact

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page/home');
 });
+
+Route::get('/contact', function(){
+    return view('page/contact');
+  });
